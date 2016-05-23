5b430f52fbeb3b0abf3d35e850b80c467e8b0e5b
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2016-05-23 20:19:36 +0100

    Aula 2016-05-23- Notes manager Node application. New note HTML form submission.

---
+++ Aula 2016-05-23/notes-app.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+
+//const notesDal = require('./data-access/local-notes-data');
+const notesDal = require('./data-access/notes-server-data');
+const notesManager = require('./model/notes-manager')(notesDal);
+const notesRouter = require('./controllers/notes-controller')(notesManager);
+
+const exphbs = require("express-handlebars");
+
+
+let hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    defaultLayout: 'main.hbs',
+    helpers: {
+        foo: function () {
+            return 'FOO!';
+        },
+        rel: function (item) {
+            return item <= 2 ? ("<b>" + item + "</b>") : item;
+        }
+    }
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+
+
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+
+app.use("/", express.static(path.join(__dirname, 'public')));
+
+app.use('/notes', notesRouter);
+
+const PORT = 3010;
+
+app.listen(PORT, function (err) {
+    console.log("Application listening on port " + PORT);
+})
+
+
